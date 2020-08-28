41c16428cddf29c8da65cbafcebf73ca12f97c22
The-Vuppukari <mohansai.2905@gmail.com>
2020-08-28 20:07:09 +0530

    Added : A file for compute ( a*b+c)

---
+++ ArthematicOperation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+read -p "Enter the Input 1:" Input1
+read -p "Enter the Input 2:" Input2
+read -p "Enter the Input 3:" Input3
+
+ArthematicOperation1=$(( Input1 + Input2 * Input3 ))
+
+ArthematicOperation1=$(( Input1 * Input2 + Input3 ))
+
