e014f750a80d3b8c5f1954300022bc5ec2a8bd47
The-Vuppukari <mohansai.2905@gmail.com>
2020-09-08 19:06:58 +0530

    Add : A code that arranges the values in array in Ascending Order

---
+++ dscArray.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -x
+
+read -p "Enter the Input 1:" Input1
+read -p "Enter the Input 2:" Input2
+read -p "Enter the Input 3:" Input3
+
+i=0
+
+ArthematicOperation1=$(( Input1 + Input2 * Input3 ))
+
+ArthematicOperation2=$(( Input1 * Input2 + Input3 ))
+
+ArthematicOperation3=$(( Input3 + Input1 / Input2 ))
+
+ArthematicOperation4=$(( Input1 % Input2 + Input3 ))
+
+declare -A Dictionary
+
+Dictionary[comp1]=$ArthematicOperation1
+Dictionary[Comp2]=$ArthematicOperation2
+Dictionary[Comp3]=$ArthematicOperation3
+Dictionary[Comp4]=$ArthematicOperation4
+
+for value in `echo ${Dictionary[@]}`
+do
+	ComputeArray[((i++))]=$value
+done
+
+for (( i=0; i<4; i++ ))
+do
+	for (( j=0; j<4-i-1; j++ ))
+	do
+		if [[ ${ComputeArray[j]} -lt ${ComputeArray[$((j+1))]} ]]
+		then
+			temp=${ComputeArray[j]}
+			ComputeArray[$j]=${ComputeArray[$((j+1))]}
+			ComputeArray[$((j+1))]=$temp
+		fi
+	done
+done
+echo ${ComputeArray[@]}
